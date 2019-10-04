eada1a9a6b3db9233c6749cf1f948217ace9c9a5
Faisal <taifaisal57@gmail.com>
2019-10-04 21:12:54 +0530

    Update Config.java

--- app/src/main/java/com/android/solulabtest/utils/Config.java
+++ app/src/main/java/com/android/solulabtest/utils/Config.java
@@ -4,7 +4,7 @@ public class Config {
     public static String LIVE_TRACKING_BROADCAST = "LIVE_TRACKING_BROADCAST";
     public static String MILESTONE_BROADCAST = "MILESTONE_BROADCAST";
     public static String IS_TRACKING_STARTED = "isStarted";
-    public static final double MILESTONE_THRESHOLD_IN_METER = 2;
+    public static final double MILESTONE_THRESHOLD_IN_METER = 50;
     public static String NOTIFICATION_CHANNEL_ID = "2001";
     public static int NOTIFICATION_ID = 101;
     public static long UPDATE_INTERVAL_IN_MILLI = 1000;
